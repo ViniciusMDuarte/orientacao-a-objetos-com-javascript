43292b82e65bdfad841ee74043707d3d2b5b5de7
Vinicius Duarte <viniduarte222@gmail.com>
2020-06-15 08:46:25 -0300

    Update index.js

--- 01_criando_objeto/index.js
+++ 01_criando_objeto/index.js
@@ -4,10 +4,14 @@ let carro = {
 }
 
 console.log(carro);
+// Object { portas: 4, cor: "Vermelho" }
+
 console.log(typeof carro);
+// Object
 
 let pessoa = {
   nome: "Gohara"
 }
 
 console.log(pessoa)
+//Gohara
